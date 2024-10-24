c8a72f5a5476556cbf0f6c356a90ce5dd3bb6f12
KaziAbdurRahim <kaziabdurrahim946@gmail.com>
2024-10-24 23:46:20 +0600

    Set up the environmrnt, navbar, footer,bannner basic

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/Header/Navbar/Navbar'
+import Banner from './components/Header/Banner/Banner'
+import Footer from './components/Footer/Footer'
+function App() {
+
+  return (
+    <>
+   
+   <Navbar></Navbar>
+    <Banner></Banner> 
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Banner/Banner.jsx
@@ -0,0 +1,24 @@
+
+
+const Banner = () => {
+    return ( 
+        <div>
+            <div className="hero hero-pattern ">
+                <div className="hero-content text-center">
+                    <img src="../../../assets/logo.png" alt="" />
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">Hello there</h1>
+                        <p className="py-6">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Banner;
