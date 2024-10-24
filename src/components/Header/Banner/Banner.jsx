b6d0ff4421e2b95bd6e17a587e9e76c7f7181711
KaziAbdurRahim <kaziabdurrahim946@gmail.com>
2024-10-25 01:28:32 +0600

    complete banner layout

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,16 +1,17 @@
 import banner_img from '../../../assets/banner-main.png'
+import bgImage from '../../../assets/bg-shadow.png'
 
 const Banner = () => {
     return ( 
-        <div>
-            <div className="hero hero-pattern ">
-                <div className="hero-content  text-center">
+        <div className='bg-black container mx-auto rounded-2xl'>
+            <div className="hero hero-pattern rounded-2xl" style={{ backgroundImage: `url(${ bgImage})`, backgroundSize: 'cover',  }}>
+                <div className="hero-content  text-center z-20">
                    
                    
-                    <div className="border   ">
+                    <div className="   ">
                        <img src={banner_img } alt="" className='  mx-auto '/>
-                        <h1 className="text-5xl font-bold">Assemble Your Ultimate Dream 11 Cricket Team</h1>
-                        <p className="py-6">
+                        <h1 className="text-5xl font-bold text-white ">Assemble Your Ultimate Dream 11 Cricket Team</h1>
+                        <p className="py-6 text-base-300">
                             Beyond Boundaries Beyond Limits
                         </p>
                         <button className="btn btn-primary bg-gradient-to-r from-red-500 to-yellow-600">Claim Free Credit</button>
