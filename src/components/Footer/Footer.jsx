1a83f1f92b6bec69d00659ca64ff9db390ae2a5e
KaziAbdurRahim <kaziabdurrahim946@gmail.com>
2024-10-25 01:07:33 +0600

    completed the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,8 @@
 
 const Footer = () => {
     return (
-        <footer className="footer bg-slate-900  text-white p-10 md:flex md:justify-between">
+        <footer className=" bg-slate-900  text-white p-10 ">
+            <div className="footer md:flex md:justify-between">
             <nav>
                 <h6 className="footer-title">About Us</h6>
              <p>We are a passionate team <br /> dedicated to providing the best <br />service to our customers.</p>
@@ -17,18 +18,24 @@ const Footer = () => {
            
             <div>
                 <h6 className="footer-title">Subscribe</h6>
-                <fieldset className="form-control w-80">
-                    <label className="label">
-                        <span className="label-text text-white">Subscribe to Our newsletter for the <br />latest updates.</span>
-                    </label>
-                    <div className="join">
+                <div className=" ">
+                    <div className="">
+                        <p className="text-white mb-2">Subscribe to Our newsletter for the <br />latest updates.</p>
+                    </div>
+                    <div className="md:join ">
                         <input
                             type="text"
                             placeholder="username@site.com"
-                            className="input input-bordered join-item" />
-                        <button className="btn btn-primary join-item  bg-gradient-to-r from-red-500 to-yellow-600">Subscribe</button>
+                            className="input input-bordered join-item " />
+                        <button className="btn btn-primary   bg-gradient-to-r from-red-500 to-yellow-600  join-item">Subscribe</button>
                     </div>
-                </fieldset>
+                </div>
+            </div>
+            </div>
+            
+            <div className="my-3">
+            <hr className=" text-white"/>
+            <p className="text-center">@2024 Cricket All Right Reserved</p>
             </div>
         </footer>
 
